18eef35239b0d84eb059e71b56953ea4b4d17470
Franklin <franklinpinedat@gmail.com>
2022-08-16 15:44:20 -0500

    hero section completed

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+export default function Hero() {
+  return (
+    <section className="hero-section">
+      <figure className="hero-section__figure">
+        <img src="../../public/photo-grid.png" alt="several photos of people doing activities"/>
+      </figure>
+      <h1 className="hero-section__title">Online Experiences</h1>
+      <p className="hero-section__description">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+      </p>
+    </section>
+  )
+}
